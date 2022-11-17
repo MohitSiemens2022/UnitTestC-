56fbdff63b1643e5ef601702beaf0588dcf5c72d
Mohit Assudani <mohit.assudani@siemens-healthineers.com>
2022-11-17 15:07:32 +0530

    Fourth commit

--- UnitTests/MathOpsTests.cs
+++ UnitTests/MathOpsTests.cs
@@ -18,20 +18,20 @@ namespace NUnitTest.UnitTests
             mathOperations = new MathOperations();
         }
 
-        [Test]
-        public void Test_Addition_2_Decimal_Numbers_Positive_Case()
+        [TestCase(100.2,200.3,300.5)]
+        [TestCase(100.2,10.255,110.455)]
+        [TestCase(-12.2, 0, -12.2)]
+        [TestCase(25.5, -55.5, -30)]
+        public void Test_Addition_2_Decimal_Numbers_Positive_Case(decimal n1, decimal n2, decimal expected)
         {
             //Arrange
-            decimal n1 = 100.2m;
-            decimal n2 = 200.3m;
-            decimal expected = 300.5m;
+            
             //Act
             decimal actual = mathOperations.Add(n1, n2);
             //Assert
             Assert.AreEqual(expected, actual);
-            Assert.Greater(n1, 0);
-            Assert.Greater(n2, 0);
-
+            //Assert.Greater(n1, 0);
+            //Assert.Greater(n2, 0);
         }
 
         [Test]
@@ -49,19 +49,20 @@ namespace NUnitTest.UnitTests
 
         }
 
-        [Test]
-        public void Test_Substraction_2_Decimal_Numbers_Positive_Case()
+        [TestCase(100.2, 200.3, -100.1)]
+        [TestCase(100.2, 10.255, 89.945)]
+        [TestCase(-12.2, 0, -12.2)]
+        [TestCase(25.5, -55.5, 80)]
+        public void Test_Substraction_2_Decimal_Numbers_Positive_Case(decimal n1,decimal n2,decimal expected)
         {
             //Arrange
-            decimal n1 = 200.3m;
-            decimal n2 = 100.2m;
-            decimal expected = 100.1m;
+        
             //Act
             decimal actual = mathOperations.Substarct(n1, n2);
             //Assert
             Assert.AreEqual(expected, actual);
-            Assert.Greater(n1, 0);
-            Assert.Greater(n2, 0);
+            //Assert.Greater(n1, 0);
+            //Greater(n2, 0);
 
         }
 
